72fb110a4362edcb58979cfc71aeedd40e55c777
André Silva Santos <andre-silvasantos200@hotmail.com>
2025-06-22 10:53:20 -0300

    dias_da_semana.c

--- olamundo.c
+++ olamundo.c
@@ -1,6 +1,44 @@
 # include <stdio.h>
 int main ()
 {
-  printf ("Ola, mundo!/n");
+    int dia;
+
+    printf("Entre com o valor do dia:\n");
+    scanf("%d", &dia);
+
+    switch (dia)
+    {
+
+    case 1:
+        printf("Domingo\n", dia);
+        break;
+
+    case 2:
+        printf("Segunda-feira\n", dia);
+        break;
+
+    case 3:
+        printf("Terça-feira\n", dia);
+        break;
+
+    case 4:
+        printf("Quarta-feira\n", dia);
+        break;
+
+    case 5:
+        printf("Quinta-feira\n", dia);
+        break;
+
+    case 6:
+        printf("Sexta-feira\n", dia);
+        break;
+
+    case 7:
+        printf("Sábado\n", dia);
+        break;
+
+    default:
+        printf("Dia inválido!\n", dia);
+    }
   return 0;
 }
